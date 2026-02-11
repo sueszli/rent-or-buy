fe88933d96eb4b5d3f90f072a120e622e09b16c4
sueszli <jabaryyahya@gmail.com>
2026-02-11 01:24:19 +0100

    up

--- src/income_tax.py
+++ src/income_tax.py
@@ -1,32 +1,3 @@
-GROSS_INCOME_BY_PERCENTILE = {
-    # data from levels.fyi
-    "10th": 28_600,
-    "25th": 43_000,
-    "50th": 58_300,
-    "75th": 74_100,
-    "90th": 91_300,
-}
-
-EXPENSES_BREAKDOWN = {
-    # vienna
-    # adjusted from numbeo.com: single, central 1bedroom
-    "Housing": 800.00,
-    "Utilities": 150.00,
-    "Groceries": 280.00,
-    "Transportation": 40.00,
-    "Subscriptions": 40.00,
-    "Discretionary": 70.00,
-    "Miscellaneous": 50.00,
-}
-
-ANNUAL_EXPENSES = sum(EXPENSES_BREAKDOWN.values()) * 12
-
-
-#
-# income tax
-#
-
-
 def _net_special(gross_special_payments: int) -> float:
     gross = float(gross_special_payments)
 
