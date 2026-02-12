37bf71d8f88e5bba160eefd54cb362ca9ae9d98a
sueszli <jabaryyahya@gmail.com>
2026-02-12 17:37:08 +0100

    up

--- src/portfolio.py
+++ src/portfolio.py
@@ -15,7 +15,7 @@ from plotnine import aes, element_text, geom_line, geom_text, ggplot, labs, scal
 # - https://curvo.eu/backtest/en
 # - https://my.oekb.at/kapitalmarkt-services/kms-output/fonds-info/sd/af/f?isin=IE00BK5BQT80 (missing lots of data)
 # - https://www.justetf.com/en/etf-profile.html?isin=IE00BK5BQT80
-# - https://www.flatex.de/fileadmin/dateien_flatex/pdf/handel/gesamtliste_premium_etfs_de.pdf (no transaction fees for flatex premium etfs)
+# - https://www.flatex.de/fileadmin/dateien_flatex/pdf/handel/gesamtliste_premium_etfs_de.pdf (no transaction fees, custody fees for flatex premium etfs)
 
 SPREAD_HALF = 0.0012 / 2  # 0.06% spread cost each way
 KEST = 0.275  # kapital ertragssteuer
@@ -29,34 +29,10 @@ def _sell_price(price: float) -> float:
     return price * (1 - SPREAD_HALF)
 
 
-def _annual_tax_data(year: int, current_price: float) -> tuple[float, float, float]:
-    # estimated year to (AgE, Korrektur, foreign) per share in EUR
-    estimated_age_rate = 0.01
-    estimated_korrektur_factor = 0.8
-    estimated_foreign_factor = 0.1
-
-    annual_tax_data = {
-        2019: (0.4, 0.32, 0.04),
-        2020: (0.4, 0.32, 0.04),
-        2021: (0.4, 0.32, 0.04),
-        2022: (0.65, 0.52, 0.07),
-        2023: (0.65, 0.52, 0.07),
-        2024: (0.35, 0.28, 0.04),
-        2025: (1.5965, 1.2962, 0.1559),
-    }
-
-    if year in annual_tax_data:
-        return annual_tax_data[year]
-
-    age = estimated_age_rate * current_price
-    korrektur = estimated_korrektur_factor * age
-    foreign = estimated_foreign_factor * age
-    return age, korrektur, foreign
-
-
-def _prices(start_year: int, start_month: int, months: int) -> list[float]:
+def _prices(months: int, start_year: int, start_month: int) -> list[float]:
     # data also embeds TER
     datapath = pathlib.Path(__file__).parent.parent / "data" / "vwce-chart.csv"
+    assert 1 * 12 <= months <= 20 * 12  # not much data available
 
     assert 1 <= start_month <= 12
     assert 2003 <= start_year <= 2024
@@ -68,17 +44,41 @@ def _prices(start_year: int, start_month: int, months: int) -> list[float]:
     return prices
 
 
-def simulate_portfolio(monthly_savings: float, years: int = 20, start_year: int = 2004, start_month: int = 1) -> pl.DataFrame:
-    assert 1 <= years <= 20  # new product, not that much historic data available
+def _annual_tax(year: int, total_shares: float, current_price: float) -> tuple[float, float, float]:
+    # estimated year to (AgE, Korrektur, foreign) per share in EUR
+    oegk_data = {
+        2019: (0.4, 0.32, 0.04),
+        2020: (0.4, 0.32, 0.04),
+        2021: (0.4, 0.32, 0.04),
+        2022: (0.65, 0.52, 0.07),
+        2023: (0.65, 0.52, 0.07),
+        2024: (0.35, 0.28, 0.04),
+        2025: (1.5965, 1.2962, 0.1559),
+    }
+
+    if year in oegk_data:
+        age_rate, step_up_rate, foreign_rate = oegk_data[year]
+    else:
+        # conservative estimate
+        age_rate = current_price * 0.015 # expect 1.5% dividend yield
+        step_up_rate = age_rate * 0.81  # prevent double taxation simulation (weighted by historic avg)
+        foreign_rate = age_rate * 0.1  # expect 10% of AgE to be creditable foreign tax
+
+    hypothetical_dividends = total_shares * age_rate
+    foreign_tax_credit = total_shares * foreign_rate
+    cost_basis_adjustment = total_shares * step_up_rate
+
+    return hypothetical_dividends, foreign_tax_credit, cost_basis_adjustment
+
+
+def simulate_portfolio(monthly_savings: float, years: int = 20, start_month: int = 1, start_year: int = 2004) -> pl.DataFrame:
     months = years * 12
-    prices = _prices(start_year, start_month, months)
+    prices = _prices(months, start_month, start_year)
 
     total_shares = 0.0
-
-    paid_tax = 0.0
     safe_from_tax = 0.0
 
-    cash_in_hand_history = []
+    payout_history = []
     dates = []
     current_date = datetime.date(start_year, start_month, 1)
 
@@ -94,24 +94,27 @@ def simulate_portfolio(monthly_savings: float, years: int = 20, start_year: int 
         # annual tax event in january
         if current_date.month == 1:
             tax_year = current_date.year - 1
-            age, korrektur, foreign = _annual_tax_data(tax_year, price)
+            hypothetical_dividends, foreign_tax_refund, national_tax_refund = _annual_tax(tax_year, total_shares, price)
 
-            hypothetical_dividends = total_shares * age
-            foreign_tax_refund = total_shares * foreign
-            national_tax_refund = total_shares * korrektur
+            # sell shares to pay tax. models opportunity cost
+            tax_due = max(0.0, hypothetical_dividends * KEST - foreign_tax_refund)
 
-            paid_tax += max(0.0, hypothetical_dividends * KEST - foreign_tax_refund)
             safe_from_tax += national_tax_refund
 
-        # assume we would liquidate today
-        # = final broker payout minus previously paid out-of-pocket annual taxes (sunk costs)
-        gross_value = total_shares * _sell_price(price)
-        profit = gross_value - safe_from_tax
-        exit_tax = profit * KEST if profit > 0 else 0.0
-        net_cash_in_hand = gross_value - exit_tax - paid_tax
-        cash_in_hand_history.append(net_cash_in_hand)
+            if tax_due > 0 and total_shares > 0:
+                shares_to_sell = tax_due / _sell_price(price)
+                safe_from_tax *= 1.0 - (shares_to_sell / total_shares)
+                total_shares -= shares_to_sell
 
-    return pl.DataFrame({"date": dates, "cash_in_hand": cash_in_hand_history})
+        # how much if we would liquidate today?
+        gross_value = total_shares * _sell_price(price)
+        taxable_value = gross_value - safe_from_tax
+        exit_tax = taxable_value * KEST if taxable_value > 0 else 0.0
+
+        payout = gross_value - exit_tax
+        payout_history.append(payout)
+
+    return pl.DataFrame({"date": dates, "payout": payout_history})
 
 
 def plot_portfolio(df: pl.DataFrame):
@@ -119,11 +122,11 @@ def plot_portfolio(df: pl.DataFrame):
     last_row = df.tail(1)
 
     p = (
-        ggplot(df, aes(x="date", y="cash_in_hand"))
+        ggplot(df, aes(x="date", y="payout"))
         + geom_line(color="#2c3e50", size=1)
         + geom_text(
             first_row,
-            aes(label="cash_in_hand"),
+            aes(label="payout"),
             va="bottom",
             ha="center",
             size=10,
@@ -132,7 +135,7 @@ def plot_portfolio(df: pl.DataFrame):
         )
         + geom_text(
             last_row,
-            aes(label="cash_in_hand"),
+            aes(label="payout"),
             va="bottom",
             ha="center",
             size=10,
@@ -157,4 +160,4 @@ def plot_portfolio(df: pl.DataFrame):
     p.show()
 
 
-# plot_portfolio(simulate_portfolio(monthly_savings=1000.0, years=20, start_year=2004, start_month=1))
+plot_portfolio(simulate_portfolio(monthly_savings=1000.0, years=20, start_year=2004, start_month=1))
